2d5739a934a0594a19e034166a76389ae26150f3
listen321 <153276853@qq.com>
2023-08-23 23:30:57 +0800

    "更新test"

--- src/main.rs
+++ src/main.rs
@@ -1484,21 +1484,493 @@
 // }
 
 // 填空
-enum Direction {
-    East,
-    West,
-    North,
-    South,
-}
+// enum Direction {
+//     East,
+//     West,
+//     North,
+//     South,
+// }
+//
+// fn main() {
+//     let dire = Direction::South;
+//     match dire {
+//         Direction::East => println!("East"),
+//         Direction::South | Direction::North => {
+//             // 在这里匹配 South 或 North
+//             println!("South or North");
+//         }
+//         _ => println!("West"),
+//     };
+// }
 
-fn main() {
-    let dire = Direction::South;
-    match dire {
-        Direction::East => println!("East"),
-        Direction::South | Direction::North => {
-            // 在这里匹配 South 或 North
-            println!("South or North");
-        }
-        _ => println!("West"),
-    };
-}
+// fn main() {
+//     let boolean = true;
+//
+//     // 使用 match 表达式填空，并满足以下条件
+//     //
+//     // boolean = true => binary = 1
+//     // boolean = false => binary = 0
+//     let binary = match boolean {
+//         true => 1,
+//         false => 0,
+//     };
+//
+//     assert_eq!(binary, 1);
+// }
+
+// enum Message {
+//     Quit,
+//     Move { x: i32, y: i32 },
+//     Write(String),
+//     ChangeColor(i32, i32, i32),
+// }
+//
+// fn main() {
+//     let msgs = [
+//         Message::Quit,
+//         Message::Move { x: 1, y: 3 },
+//         Message::ChangeColor(255, 255, 0),
+//     ];
+//
+//     for msg in msgs {
+//         show_message(msg)
+//     }
+// }
+//
+// fn show_message(msg: Message) {
+//     match msg {
+//         Message::Move { x: a, y: b } => {
+//             // match  Message::Move
+//             assert_eq!(a, 1);
+//             assert_eq!(b, 3);
+//         }
+//         Message::ChangeColor(_, g, b) => {
+//             assert_eq!(g, 255);
+//             assert_eq!(b, 0);
+//         }
+//         _ => println!("no data in these variants"),
+//     }
+// }
+
+// fn main() {
+//     let alphabets = ['a', 'E', 'Z', '0', 'x', '9', 'Y'];
+//
+//     // 使用 `matches` 填空
+//     for ab in alphabets {
+//         assert!(matches!(ab, 'a'..='z' | 'A'..='Z' | '0'..='9'))
+//     }
+// }
+
+// enum MyEnum {
+//     Foo,
+//     Bar,
+// }
+//
+// fn main() {
+//     let mut count = 0;
+//
+//     let v = vec![MyEnum::Foo, MyEnum::Bar, MyEnum::Foo];
+//     for e in v {
+//         if matches!(e, MyEnum::Foo) {
+//             // 修复错误，只能修改本行代码
+//             count += 1;
+//         }
+//     }
+//
+//     assert_eq!(count, 2);
+// }
+
+// fn main() {
+//     let o = Some(7);
+//
+//     if let Some(i) = o {
+//         println!("This is a really long string and `{:?}`", i);
+//     }
+//     // 移除整个 `match` 语句块，使用 `if let` 替代
+//     // match o {
+//     //     Some(i) => {
+//     //         println!("This is a really long string and `{:?}`", i);
+//     //     }
+//     //     _ => {}
+//     // };
+// }
+
+// 填空
+// enum Foo {
+//     Bar(u8),
+// }
+//
+// fn main() {
+//     let a = Foo::Bar(1);
+//
+//     if let Foo::Bar(i) = a {
+//         println!("foobar 持有的值是: {}", i);
+//     }
+// }
+
+// enum Foo {
+//     Bar,
+//     Baz,
+//     Qux(u32),
+// }
+//
+// fn main() {
+//     let a = Foo::Qux(10);
+//     match a {
+//         Foo::Bar => {
+//             println!("match foo::bar")
+//         }
+//         Foo::Baz => {
+//             println!("match foo::baz")
+//         }
+//         __ => {
+//             println!("match others")
+//         }
+//     }
+//     // 移除以下代码，使用 `match` 代替
+//     // if let Foo::Bar = a {
+//     //     println!("match foo::bar")
+//     // } else if let Foo::Baz = a {
+//     //     println!("match foo::baz")
+//     // } else {
+//     //     println!("match others")
+//     // }
+// }
+
+// fn main() {
+//     let age = Some(30);
+//     if let Some(age) = age {
+//         // create a new variable with the same name as previous `age`
+//         assert_eq!(age, 30);
+//     } // the new variable `age` goes out of scope here
+//
+//     match age {
+//         // match can also introduce a new shadowed variable
+//         Some(age) => println!("age is a new variable, it's value is {}", age),
+//         _ => (),
+//     }
+// }
+
+// fn main() {
+//     match_number(5)
+// }
+// fn match_number(n: i32) {
+//     match n {
+//         // 匹配一个单独的值
+//         1 => println!("One!"),
+//         // 使用 `|` 填空，不要使用 `..` 或 `..=`
+//         2..=5 => println!("match 2 -> 5"),
+//         // 匹配一个闭区间的数值序列
+//         6..=10 => {
+//             println!("match 6 -> 10")
+//         }
+//         _ => {
+//             println!("match 11 -> +infinite")
+//         }
+//     }
+// }
+
+// struct Point {
+//     x: i32,
+//     y: i32,
+// }
+//
+// fn main() {
+//     // 填空，让 p 匹配第二个分支
+//     let p = Point { x: 1, y: 20 };
+//
+//     match p {
+//         Point { x, y: 0 } => println!("On the x axis at {}", x),
+//         // 第二个分支
+//         Point {
+//             x: 0..=5,
+//             y: y @ (10 | 20 | 30),
+//         } => println!("On the y axis at {}", y),
+//         Point { x, y } => println!("On neither axis: ({}, {})", x, y),
+//     }
+// }
+
+// 修复错误
+// enum Message {
+//     Hello { id: i32 },
+// }
+//
+// fn main() {
+//     let msg = Message::Hello { id: 5 };
+//
+//     match msg {
+//         Message::Hello { id: id @ 3..=7 } => println!("id 值的范围在 [3, 7] 之间: {}", id),
+//         Message::Hello {
+//             id: newid @ (10 | 11 | 12),
+//         } => {
+//             println!("id 值的范围在 [10, 12] 之间: {}", newid)
+//         }
+//         Message::Hello { id } => println!("Found some other id: {}", id),
+//     }
+// }
+
+// 填空让代码工作，必须使用 `split`
+// fn main() {
+//     let num = Some(8);
+//     let split = 5;
+//     match num {
+//         Some(x) if x < split => assert!(x < split),
+//         Some(x) => assert!(x >= split),
+//         None => (),
+//     }
+// }
+
+// 填空，让代码工作
+// fn main() {
+//     let numbers = (2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048);
+//
+//     match numbers {
+//         (first, .., last) => {
+//             assert_eq!(first, 2);
+//             assert_eq!(last, 2048);
+//         }
+//     }
+// }
+
+// 修复错误，尽量少地修改代码
+// 不要移除任何代码行
+// fn main() {
+//     let mut v = String::from("hello,");
+//     let r = &mut v;
+//
+//     match r {
+//         value => value.push_str(" world!"),
+//     }
+//     println!("{v}")
+// }
+
+// struct Point {
+//     x: f64,
+//     y: f64,
+// }
+//
+// // `Point` 的关联函数都放在下面的 `impl` 语句块中
+// impl Point {
+//     // 关联函数的使用方法跟构造器非常类似
+//     fn origin() -> Point {
+//         Point { x: 0.0, y: 0.0 }
+//     }
+//
+//     // 另外一个关联函数，有两个参数
+//     fn new(x: f64, y: f64) -> Point {
+//         Point { x: x, y: y }
+//     }
+// }
+//
+// struct Rectangle {
+//     p1: Point,
+//     p2: Point,
+// }
+//
+// impl Rectangle {
+//     // 这是一个方法
+//     // `&self` 是 `self: &Self` 的语法糖
+//     // `Self` 是当前调用对象的类型，对于本例来说 `Self` = `Rectangle`
+//     fn area(&self) -> f64 {
+//         // 使用点操作符可以访问 `self` 中的结构体字段
+//         let Point { x: x1, y: y1 } = self.p1;
+//         let Point { x: x2, y: y2 } = self.p2;
+//
+//         // `abs` 是一个 `f64` 类型的方法，会返回调用者的绝对值
+//         ((x1 - x2) * (y1 - y2)).abs()
+//     }
+//
+//     fn perimeter(&self) -> f64 {
+//         let Point { x: x1, y: y1 } = self.p1;
+//         let Point { x: x2, y: y2 } = self.p2;
+//
+//         2.0 * ((x1 - x2).abs() + (y1 - y2).abs())
+//     }
+//
+//     // 该方法要求调用者是可变的，`&mut self` 是 `self: &mut Self` 的语法糖
+//     fn translate(&mut self, x: f64, y: f64) {
+//         self.p1.x += x;
+//         self.p2.x += x;
+//
+//         self.p1.y += y;
+//         self.p2.y += y;
+//     }
+// }
+//
+// // `Pair` 持有两个分配在堆上的整数
+// struct Pair(Box<i32>, Box<i32>);
+//
+// impl Pair {
+//     // 该方法会拿走调用者的所有权
+//     // `self` 是 `self: Self` 的语法糖
+//     fn destroy(self) {
+//         let Pair(first, second) = self;
+//
+//         println!("Destroying Pair({}, {})", first, second);
+//
+//         // `first` 和 `second` 在这里超出作用域并被释放
+//     }
+// }
+//
+// fn main() {
+//     let rectangle = Rectangle {
+//         // 关联函数的调用不是通过点操作符，而是使用 `::`
+//         p1: Point::origin(),
+//         p2: Point::new(3.0, 4.0),
+//     };
+//
+//     // 方法才是通过点操作符调用
+//     // 注意，这里的方法需要的是 `&self` 但是我们并没有使用 `(&rectangle).perimeter()` 来调用，原因在于：
+//     // 编译器会帮我们自动取引用
+//     //  `rectangle.perimeter()` === `Rectangle::perimeter(&rectangle)`
+//     println!("Rectangle perimeter: {}", rectangle.perimeter());
+//     println!("Rectangle area: {}", rectangle.area());
+//
+//     let mut square = Rectangle {
+//         p1: Point::origin(),
+//         p2: Point::new(1.0, 1.0),
+//     };
+//
+//     // 错误！`rectangle` 是不可变的，但是这个方法要求一个可变的对象
+//     // rectangle.translate(1.0, 0.0);
+//     // TODO ^ 试着反注释此行，看看会发生什么
+//
+//     // 可以！可变对象可以调用可变的方法
+//     square.translate(1.0, 1.0);
+//
+//     let pair = Pair(Box::new(1), Box::new(2));
+//
+//     pair.destroy();
+//
+//     // Error! 上一个 `destroy` 调用拿走了 `pair` 的所有权
+//     // pair.destroy();
+//     // TODO ^ 试着反注释此行
+// }
+
+// struct Rectangle {
+//     width: u32,
+//     height: u32,
+// }
+//
+// impl Rectangle {
+//     // 完成 area 方法，返回矩形 Rectangle 的面积
+//     fn area(self) -> u32 {
+//         self.width * self.height
+//     }
+// }
+//
+// fn main() {
+//     let rect1 = Rectangle {
+//         width: 30,
+//         height: 50,
+//     };
+//
+//     assert_eq!(rect1.area(), 1500);
+// }
+
+// 只填空，不要删除任何代码行!
+// #[derive(Debug)]
+// struct TrafficLight {
+//     color: String,
+// }
+//
+// impl TrafficLight {
+//     pub fn show_state(&self) {
+//         println!("the current state is {}", self.color);
+//     }
+// }
+// fn main() {
+//     let light = TrafficLight {
+//         color: "red".to_owned(),
+//     };
+//     // 不要拿走 `light` 的所有权
+//     light.show_state();
+//     // 否则下面代码会报错
+//     println!("{:?}", light);
+// }
+
+// struct TrafficLight {
+//     color: String,
+// }
+//
+// impl TrafficLight {
+//     // 使用 `Self` 填空
+//     pub fn show_state(self: &self) {
+//         println!("the current state is {}", &self.color);
+//     }
+//
+//     // 填空，不要使用 `Self` 或其变体
+//     pub fn change_state(&mut self) {
+//         self.color = "green".to_string()
+//     }
+// }
+// fn main() {}
+
+// #[derive(Debug)]
+// struct TrafficLight {
+//     color: String,
+// }
+//
+// impl TrafficLight {
+//     // 1. 实现下面的关联函数 `new`,
+//     // 2. 该函数返回一个 TrafficLight 实例，包含 `color` "red"
+//     // 3. 该函数必须使用 `Self` 作为类型，不能在签名或者函数体中使用 `TrafficLight`
+//     pub fn new(color: String) -> Self {
+//         Self { color: color }
+//     }
+//
+//     pub fn get_state(&self) -> &str {
+//         &self.color
+//     }
+// }
+//
+// fn main() {
+//     let light = TrafficLight::new("red".to_string());
+//     assert_eq!(light.get_state(), "red");
+// }
+
+// struct Rectangle {
+//     width: u32,
+//     height: u32,
+// }
+//
+// // 使用多个 `impl` 语句块重写下面的代码
+// impl Rectangle {
+//     fn area(&self) -> u32 {
+//         self.width * self.height
+//     }
+// }
+//
+// impl Rectangle {
+//     fn can_hold(&self, other: &Rectangle) -> bool {
+//         self.width > other.width && self.height > other.height
+//     }
+// }
+//
+// fn main() {}
+
+// #[derive(Debug)]
+// enum TrafficLightColor {
+//     Red,
+//     Yellow,
+//     Green,
+// }
+//
+// // 为 TrafficLightColor 实现所需的方法
+// impl TrafficLightColor {
+//     fn color(&self) -> String {
+//         match *self {
+//             TrafficLightColor::Red => "red".to_string(),
+//             TrafficLightColor::Yellow => "yellow".to_string(),
+//             TrafficLightColor::Green => "green".to_string(),
+//         }
+//     }
+// }
+//
+// fn main() {
+//     let c = TrafficLightColor::Yellow;
+//
+//     assert_eq!(c.color(), "yellow");
+//
+//     println!("{:?}", c);
+// }
