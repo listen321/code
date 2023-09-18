684223b7105d65766fa05ae89e8b39e9dc53d66a
listen321 <153276853@qq.com>
2023-09-18 23:45:40 +0800

    add test

--- src/main.rs
+++ src/main.rs
@@ -1974,3 +1974,890 @@
 //
 //     println!("{:?}", c);
 // }
+
+// 填空
+// struct A; // 具体的类型 `A`.
+// struct S(A); // 具体的类型 `S`.
+// struct SGen<T>(T); // 泛型 `SGen`.
+//
+// fn reg_fn(_s: S) {}
+//
+// fn gen_spec_t(_s: SGen<A>) {}
+//
+// fn gen_spec_i32(_s: SGen<i32>) {}
+//
+// fn generic<T>(_s: SGen<T>) {}
+//
+// fn main() {
+//     // 使用非泛型函数
+//     reg_fn(S(A)); // 具体的类型
+//     gen_spec_t(SGen(A)); // 隐式地指定类型参数  `A`.
+//     gen_spec_i32(SGen(1)); // 隐式地指定类型参数`i32`.
+//
+//     // 显式地指定类型参数 `char`
+//     generic::<char>(SGen('a'));
+//
+//     // 隐式地指定类型参数 `char`.
+//     generic(SGen('a'));
+// }
+
+// 实现下面的泛型函数 sum
+// fn sum<T: std::ops::Add<Output = T>>(x: T, y: T) -> T {
+//     return x + y;
+// }
+//
+// fn main() {
+//     assert_eq!(5, sum(2i8, 3i8));
+//     assert_eq!(50, sum(20, 30));
+//     assert_eq!(2.46, sum(1.23, 1.23));
+// }
+
+// 实现一个结构体 Point 让代码工作
+// struct Point<T> {
+//     x: T,
+//     y: T,
+// }
+//
+// fn main() {
+//     let integer = Point { x: 5, y: 10 };
+//     let float = Point { x: 1.0, y: 4.0 };
+// }
+
+// 修改以下结构体让代码工作
+// struct Point<T, W> {
+//     x: T,
+//     y: W,
+// }
+//
+// fn main() {
+//     // 不要修改这行代码！
+//     let p = Point {
+//         x: 5,
+//         y: "hello".to_string(),
+//     };
+// }
+
+// 为 Val 增加泛型参数，不要修改 `main` 中的代码
+// struct Val<T> {
+//     val: T,
+// }
+//
+// impl<T> Val<T> {
+//     fn value(&self) -> &T {
+//         &self.val
+//     }
+// }
+//
+// fn main() {
+//     let x = Val { val: 3.0 };
+//     let y = Val {
+//         val: "hello".to_string(),
+//     };
+//     println!("{}, {}", x.value(), y.value());
+// }
+
+// struct Point<T, U> {
+//     x: T,
+//     y: U,
+// }
+//
+// impl<T, U> Point<T, U> {
+//     // 实现 mixup，不要修改其它代码！
+//     fn mixup<V, W>(self, point: Point<V, W>) -> Point<T, W> {
+//         return Point {
+//             x: self.x,
+//             y: point.y,
+//         };
+//     }
+// }
+//
+// fn main() {
+//     let p1 = Point { x: 5, y: 10 };
+//     let p2 = Point {
+//         x: "Hello",
+//         y: '中',
+//     };
+//
+//     let p3 = p1.mixup(p2);
+//
+//     assert_eq!(p3.x, 5);
+//     assert_eq!(p3.y, '中');
+// }
+
+// 修复错误，让代码工作
+// struct Point<T> {
+//     x: T,
+//     y: T,
+// }
+//
+// impl Point<f32> {
+//     fn distance_from_origin(&self) -> f32 {
+//         (self.x.powi(2) + self.y.powi(2)).sqrt()
+//     }
+// }
+//
+// fn main() {
+//     let p = Point {
+//         x: 5_f32,
+//         y: 10_f32,
+//     };
+//     println!("{}", p.distance_from_origin());
+// }
+//
+//
+//
+// struct ArrayPair<T, const N: usize> {
+//     left: [T; N],
+//     right: [T; N],
+// }
+//
+// // impl<T: Debug, const N: usize> Debug for ArrayPair<T, N> {
+// //     // ...
+// // }
+//
+// fn foo<const N: usize>() {}
+//
+// fn bar<T, const M: usize>() {
+//     foo::<M>(); // ok: 符合第一种
+//     foo::<2021>(); // ok: 符合第二种
+//     foo::<{ 20 * 100 + 20 * 10 + 1 }>(); // ok: 符合第三种
+//
+//     // foo::<{ M + 1 }>(); // error: 违背第三种，const 表达式中不能有泛型参数 M
+//     // foo::<{ std::mem::size_of::<T>() }>(); // error: 泛型表达式包含了泛型参数 T
+//
+//     let _: [u8; M]; // ok: 符合第一种
+//                     // let _: [u8; std::mem::size_of::<T>()]; // error: 泛型表达式包含了泛型参数 T
+// }
+//
+// fn main() {}
+//
+//
+
+// pub struct MinSlice<T, const N: usize> {
+//     pub head: [T; N],
+//     pub tail: [T],
+// }
+//
+// fn main() {
+//     let slice: &[u8] = b"Hello, world";
+//     let reference: Option<&u8> = slice.get(6);
+//     // 我们知道 `.get` 返回的是 `Some(b' ')`
+//     // 但编译器不知道
+//     assert!(reference.is_some());
+//
+//     let slice: &[u8] = b"Hello, world";
+//
+//     // 当编译构建 MinSlice 时会进行长度检查，也就是在编译期我们就知道它的长度是 12
+//     // 在运行期，一旦 `unwrap` 成功，在 `MinSlice` 的作用域内，就再无需任何检查
+//     let minslice = MinSlice::<u8, 12>::from_slice(slice).unwrap();
+//     let value: u8 = minslice.head[6];
+//     assert_eq!(value, b' ')
+// }
+
+// 修复错误
+// struct Array<T, const N: usize> {
+//     data: [T; N],
+// }
+//
+// fn main() {
+//     let arrays = [
+//         Array { data: [1, 2, 3] },
+//         Array { data: [1, 2, 3] },
+//         Array { data: [1, 2, 5] },
+//     ];
+// }
+
+// 填空
+// fn print_array<T: std::fmt::Debug, const N: usize>(arr: [T; N]) {
+//     println!("{:?}", arr);
+// }
+// fn main() {
+//     let arr = [1, 2, 3];
+//     print_array(arr);
+//
+//     let arr = ["hello", "world"];
+//     print_array(arr);
+// }
+
+// #![allow(incomplete_features)]
+// #![feature(generic_const_exprs)]
+//
+// fn check_size<T>(val: T)
+// where
+//     Assert<{ core::mem::size_of::<T>() < 768 }>: IsTrue,
+// {
+//     //...
+// }
+//
+// // 修复 main 函数中的错误
+// fn main() {
+//     check_size([0u8; 767]);
+//     check_size([0i32; 100]);
+//     check_size(["hello你好"; 10]); // size of &str ?
+//     check_size([(); 4].map(|_| "hello你好".to_string())); // size of String?
+//     check_size(['中'; 141]); // size of char ?
+// }
+//
+// pub enum Assert<const CHECK: bool> {}
+//
+// pub trait IsTrue {}
+//
+// impl IsTrue for Assert<true> {}
+
+// struct Sheep {
+//     naked: bool,
+//     name: String,
+// }
+//
+// impl Sheep {
+//     fn is_naked(&self) -> bool {
+//         self.naked
+//     }
+//
+//     fn shear(&mut self) {
+//         if self.is_naked() {
+//             // `Sheep` 结构体上定义的方法可以调用 `Sheep` 所实现的特征的方法
+//             println!("{} is already naked...", self.name());
+//         } else {
+//             println!("{} gets a haircut!", self.name);
+//
+//             self.naked = true;
+//         }
+//     }
+// }
+//
+// trait Animal {
+//     // 关联函数签名；`Self` 指代实现者的类型
+//     // 例如我们在为 Pig 类型实现特征时，那 `new` 函数就会返回一个 `Pig` 类型的实例，这里的 `Self` 指代的就是 `Pig` 类型
+//     fn new(name: String) -> Self;
+//
+//     // 方法签名
+//     fn name(&self) -> String;
+//
+//     fn noise(&self) -> String;
+//
+//     // 方法还能提供默认的定义实现
+//     fn talk(&self) {
+//         println!("{} says {}", self.name(), self.noise());
+//     }
+// }
+//
+// impl Animal for Sheep {
+//     // `Self` 被替换成具体的实现者类型： `Sheep`
+//     fn new(name: String) -> Sheep {
+//         Sheep {
+//             name: name,
+//             naked: false,
+//         }
+//     }
+//
+//     fn name(&self) -> String {
+//         self.name.clone()
+//     }
+//
+//     fn noise(&self) -> String {
+//         if self.is_naked() {
+//             "baaaaah?".to_string()
+//         } else {
+//             "baaaaah!".to_string()
+//         }
+//     }
+//
+//     // 默认的特征方法可以被重写
+//     fn talk(&self) {
+//         println!("{} pauses briefly... {}", self.name, self.noise());
+//     }
+// }
+//
+// fn main() {
+//     // 这里的类型注释时必须的
+//     let mut dolly: Sheep = Animal::new("Dolly".to_string());
+//     // TODO ^ 尝试去除类型注释，看看会发生什么
+//
+//     dolly.talk();
+//     dolly.shear();
+//     dolly.talk();
+// }
+
+// 完成两个 `impl` 语句块
+// 不要修改 `main` 中的代码
+// trait Hello {
+//     fn say_hi(&self) -> String {
+//         String::from("hi")
+//     }
+//
+//     fn say_something(&self) -> String;
+// }
+//
+// struct Student {}
+// impl Hello for Student {
+//     fn say_something(&self) -> String {
+//         String::from("I'm a good student")
+//     }
+// }
+// struct Teacher {}
+// impl Hello for Teacher {
+//     fn say_hi(&self) -> String {
+//         String::from("Hi, I'm your new teacher")
+//     }
+//
+//     fn say_something(&self) -> String {
+//         String::from("I'm not a bad teacher")
+//     }
+// }
+//
+// fn main() {
+//     let s = Student {};
+//     assert_eq!(s.say_hi(), "hi");
+//     assert_eq!(s.say_something(), "I'm a good student");
+//
+//     let t = Teacher {};
+//     assert_eq!(t.say_hi(), "Hi, I'm your new teacher");
+//     assert_eq!(t.say_something(), "I'm not a bad teacher");
+//
+//     println!("Success!")
+// }
+
+// `Centimeters`, 一个元组结构体，可以被比较大小
+// #[derive(PartialEq, PartialOrd)]
+// struct Centimeters(f64);
+//
+// // `Inches`, 一个元组结构体可以被打印
+// #[derive(Debug)]
+// struct Inches(i32);
+//
+// impl Inches {
+//     fn to_centimeters(&self) -> Centimeters {
+//         let &Inches(inches) = self;
+//
+//         Centimeters(inches as f64 * 2.54)
+//     }
+// }
+//
+// // 添加一些属性让代码工作
+// // 不要修改其它代码！
+// #[derive(Debug, PartialEq, PartialOrd)]
+// struct Seconds(i32);
+//
+// fn main() {
+//     let _one_second = Seconds(1);
+//
+//     println!("One second looks like: {:?}", _one_second);
+//     let _this_is_true = _one_second == _one_second;
+//     let _this_is_true = _one_second > _one_second;
+//
+//     let foot = Inches(12);
+//
+//     println!("One foot equals {:?}", foot);
+//
+//     let meter = Centimeters(100.0);
+//
+//     let cmp = if foot.to_centimeters() < meter {
+//         "smaller"
+//     } else {
+//         "bigger"
+//     };
+//
+//     println!("One foot is {} than one meter.", cmp);
+// }
+
+// use std::ops;
+//
+// // 实现 fn multiply 方法
+// // 如上所述，`+` 需要 `T` 类型实现 `std::ops::Add` 特征
+// // 那么, `*` 运算符需要实现什么特征呢? 你可以在这里找到答案: https://doc.rust-lang.org/core/ops/
+// fn multiply<T: ops::Mul<Output = T>>(x: T, y: T) -> T {
+//     x * y
+// }
+//
+// fn main() {
+//     assert_eq!(6, multiply(2u8, 3u8));
+//     assert_eq!(5.0, multiply(1.0, 5.0));
+//
+//     println!("Success!")
+// }
+//
+
+// 修复错误，不要修改 `main` 中的代码!
+// use std::ops;
+//
+// struct Foo;
+// struct Bar;
+//
+// #[derive(Debug, PartialEq)]
+// struct FooBar;
+//
+// #[derive(Debug, PartialEq)]
+// struct BarFoo;
+//
+// // 下面的代码实现了自定义类型的相加： Foo + Bar = FooBar
+// impl ops::Add<Bar> for Foo {
+//     type Output = FooBar;
+//
+//     fn add(self, _rhs: Bar) -> FooBar {
+//         FooBar
+//     }
+// }
+//
+// impl ops::Sub<Bar> for Foo {
+//     type Output = BarFoo;
+//
+//     fn sub(self, _rhs: Bar) -> BarFoo {
+//         BarFoo
+//     }
+// }
+//
+// fn main() {
+//     // 不要修改下面代码
+//     // 你需要为 FooBar 派生一些特征来让代码工作
+//     assert_eq!(Foo + Bar, FooBar);
+//     assert_eq!(Foo - Bar, BarFoo);
+//
+//     println!("Success!")
+// }
+
+// 实现 `fn summary`
+// 修复错误且不要移除任何代码行
+// trait Summary {
+//     fn summarize(&self) -> String;
+// }
+//
+// #[derive(Debug)]
+// struct Post {
+//     title: String,
+//     author: String,
+//     content: String,
+// }
+//
+// impl Summary for Post {
+//     fn summarize(&self) -> String {
+//         format!("The author of post {} is {}", self.title, self.author)
+//     }
+// }
+//
+// #[derive(Debug)]
+// struct Weibo {
+//     username: String,
+//     content: String,
+// }
+//
+// impl Summary for Weibo {
+//     fn summarize(&self) -> String {
+//         format!("{} published a weibo {}", self.username, self.content)
+//     }
+// }
+//
+// fn main() {
+//     let post = Post {
+//         title: "Popular Rust".to_string(),
+//         author: "Sunface".to_string(),
+//         content: "Rust is awesome!".to_string(),
+//     };
+//     let weibo = Weibo {
+//         username: "sunface".to_string(),
+//         content: "Weibo seems to be worse than Tweet".to_string(),
+//     };
+//
+//     summary(&post);
+//     summary(&weibo);
+//
+//     println!("{:?}", post);
+//     println!("{:?}", weibo);
+// }
+//
+// // 在下面实现 `fn summary` 函数
+// fn summary(t: &impl Summary) -> String {
+//     t.summarize()
+// }
+
+// struct Sheep {}
+// struct Cow {}
+//
+// trait Animal {
+//     fn noise(&self) -> String;
+// }
+//
+// impl Animal for Sheep {
+//     fn noise(&self) -> String {
+//         "baaaaah!".to_string()
+//     }
+// }
+//
+// impl Animal for Cow {
+//     fn noise(&self) -> String {
+//         "moooooo!".to_string()
+//     }
+// }
+//
+// // 返回一个类型，该类型实现了 Animal 特征，但是我们并不能在编译期获知具体返回了哪个类型
+// // 修复这里的错误，你可以使用虚假的随机，也可以使用特征对象
+// //fn random_animal(random_number: f64) -> impl Animal {
+// fn random_animal(random_number: f64) -> Box<dyn Animal> {
+//     if random_number < 0.5 {
+//         Box::new(Sheep {})
+//     } else {
+//         Box::new(Cow {})
+//     }
+// }
+//
+// fn main() {
+//     let random_number = 0.234;
+//     let animal = random_animal(random_number);
+//     println!(
+//         "You've randomly chosen an animal, and it says {}",
+//         animal.noise()
+//     );
+// }
+//
+
+// fn main() {
+//     assert_eq!(sum(1, 2), 3);
+// }
+//
+// // 通过两种方法使用特征约束来实现 `fn sum`
+// fn sum<T: std::ops::Add<Output = T>>(x: T, y: T) -> T {
+//     x + y
+// }
+//
+
+// 修复代码中的错误
+// struct Pair<T> {
+//     x: T,
+//     y: T,
+// }
+//
+// impl<T> Pair<T> {
+//     fn new(x: T, y: T) -> Self {
+//         Self { x, y }
+//     }
+// }
+//
+// impl<T: std::fmt::Debug + PartialOrd> Pair<T> {
+//     fn cmp_display(&self) {
+//         if self.x >= self.y {
+//             println!("The largest member is x = {:?}", self.x);
+//         } else {
+//             println!("The largest member is y = {:?}", self.y);
+//         }
+//     }
+// }
+//
+// #[derive(Debug, PartialEq, PartialOrd)]
+// struct Unit(i32);
+//
+// fn main() {
+//     let pair = Pair {
+//         x: Unit(1),
+//         y: Unit(3),
+//     };
+//
+//     pair.cmp_display();
+// }
+
+// 填空
+// fn example1() {
+//     // `T: Trait` 是最常使用的方式
+//     // `T: Fn(u32) -> u32` 说明 `T` 只能接收闭包类型的参数
+//     struct Cacher<T: Fn(u32) -> u32> {
+//         calculation: T,
+//         value: Option<u32>,
+//     }
+//
+//     impl<T: Fn(u32) -> u32> Cacher<T> {
+//         fn new(calculation: T) -> Cacher<T> {
+//             Cacher {
+//                 calculation,
+//                 value: None,
+//             }
+//         }
+//
+//         fn value(&mut self, arg: u32) -> u32 {
+//             match self.value {
+//                 Some(v) => v,
+//                 None => {
+//                     let v = (self.calculation)(arg);
+//                     self.value = Some(v);
+//                     v
+//                 }
+//             }
+//         }
+//     }
+//
+//     let mut cacher = Cacher::new(|x| x + 1);
+//     assert_eq!(cacher.value(10), 11);
+//     assert_eq!(cacher.value(15), 11);
+// }
+//
+// fn example2() {
+//     // 还可以使用 `where` 来约束 T
+//     struct Cacher<T>
+//     where
+//         T: Fn(u32) -> u32,
+//     {
+//         calculation: T,
+//         value: Option<u32>,
+//     }
+//
+//     impl<T> Cacher<T>
+//     where
+//         T: Fn(u32) -> u32,
+//     {
+//         fn new(calculation: T) -> Cacher<T> {
+//             Cacher {
+//                 calculation,
+//                 value: None,
+//             }
+//         }
+//
+//         fn value(&mut self, arg: u32) -> u32 {
+//             match self.value {
+//                 Some(v) => v,
+//                 None => {
+//                     let v = (self.calculation)(arg);
+//                     self.value = Some(v);
+//                     v
+//                 }
+//             }
+//         }
+//     }
+//
+//     let mut cacher = Cacher::new(|x| x + 1);
+//     assert_eq!(cacher.value(20), 21);
+//     assert_eq!(cacher.value(25), 21);
+// }
+//
+// fn main() {
+//     example1();
+//     example2();
+//
+//     println!("Success!")
+// }
+
+// trait Bird {
+//     fn quack(&self) -> String;
+// }
+//
+// struct Duck;
+// impl Duck {
+//     fn swim(&self) {
+//         println!("Look, the duck is swimming")
+//     }
+// }
+// struct Swan;
+// impl Swan {
+//     fn fly(&self) {
+//         println!("Look, the duck.. oh sorry, the swan is flying")
+//     }
+// }
+//
+// impl Bird for Duck {
+//     fn quack(&self) -> String {
+//         "duck duck".to_string()
+//     }
+// }
+//
+// impl Bird for Swan {
+//     fn quack(&self) -> String {
+//         "swan swan".to_string()
+//     }
+// }
+//
+// fn main() {
+//     // 填空
+//     let duck = Duck;
+//     duck.swim();
+//
+//     let bird = hatch_a_bird(2);
+//     // 变成鸟儿后，它忘记了如何游，因此以下代码会报错
+//     // bird.swim();
+//     // 但它依然可以叫唤
+//     assert_eq!(bird.quack(), "duck duck");
+//
+//     let bird = hatch_a_bird(1);
+//     // 这只鸟儿忘了如何飞翔，因此以下代码会报错
+//     // bird.fly();
+//     // 但它也可以叫唤
+//     assert_eq!(bird.quack(), "swan swan");
+//
+//     println!("Success!")
+// }
+//
+// // 实现以下函数
+// fn hatch_a_bird(x: i32) -> Box<dyn Bird> {
+//     match x {
+//         1 => Box::new(Swan {}),
+//         2 => Box::new(Duck {}),
+//         _ => Box::new(Duck {}),
+//     }
+// }
+
+// trait Bird {
+//     fn quack(&self);
+// }
+//
+// struct Duck;
+// impl Duck {
+//     fn fly(&self) {
+//         println!("Look, the duck is flying")
+//     }
+// }
+// struct Swan;
+// impl Swan {
+//     fn fly(&self) {
+//         println!("Look, the duck.. oh sorry, the swan is flying")
+//     }
+// }
+//
+// impl Bird for Duck {
+//     fn quack(&self) {
+//         println!("{}", "duck duck");
+//     }
+// }
+//
+// impl Bird for Swan {
+//     fn quack(&self) {
+//         println!("{}", "swan swan");
+//     }
+// }
+//
+// fn main() {
+//     // 填空
+//     let birds: [Box<dyn Bird>; 2] = [Box::new(Duck {}), Box::new(Swan {})];
+//
+//     for bird in birds {
+//         bird.quack();
+//         // 当 duck 和 swan 变成 bird 后，它们都忘了如何翱翔于天际，只记得该怎么叫唤了。。
+//         // 因此，以下代码会报错
+//         // bird.fly();
+//     }
+// }
+
+// 填空
+// trait Draw {
+//     fn draw(&self) -> String;
+// }
+//
+// impl Draw for u8 {
+//     fn draw(&self) -> String {
+//         format!("u8: {}", *self)
+//     }
+// }
+//
+// impl Draw for f64 {
+//     fn draw(&self) -> String {
+//         format!("f64: {}", *self)
+//     }
+// }
+//
+// fn main() {
+//     let x = 1.1f64;
+//     let y = 8u8;
+//
+//     // draw x
+//     draw_with_box(Box::new(x));
+//
+//     // draw y
+//     draw_with_ref(&y);
+//
+//     println!("Success!")
+// }
+//
+// fn draw_with_box(x: Box<dyn Draw>) {
+//     println!("{}", x.draw());
+// }
+//
+// fn draw_with_ref(x: &dyn Draw) {
+//     println!("{}", x.draw());
+// }
+
+// trait Foo {
+//     fn method(&self) -> String;
+// }
+//
+// impl Foo for u8 {
+//     fn method(&self) -> String {
+//         format!("u8: {}", *self)
+//     }
+// }
+//
+// impl Foo for String {
+//     fn method(&self) -> String {
+//         format!("string: {}", *self)
+//     }
+// }
+//
+// // 通过泛型实现以下函数
+// fn static_dispatch<T: Foo>(x: T) {
+//     println!("{}", x.method())
+// }
+//
+// // 通过特征对象实现以下函数
+// fn dynamic_dispatch(x: &dyn Foo) {
+//     println!("{}", x.method())
+// }
+//
+// fn main() {
+//     let x = 5u8;
+//     let y = "Hello".to_string();
+//
+//     static_dispatch(x);
+//     dynamic_dispatch(&y);
+//
+//     println!("Success!")
+// }
+
+// 使用至少两种方法让代码工作
+// 不要添加/删除任何代码行
+// trait MyTrait {
+//     fn f(&self) -> Box<dyn MyTrait>;
+// }
+//
+// impl MyTrait for u32 {
+//     fn f(&self) -> Box<dyn MyTrait> {
+//         Box::new(42)
+//     }
+// }
+//
+// impl MyTrait for String {
+//     fn f(&self) -> Box<dyn MyTrait> {
+//         Box::new(self.clone())
+//     }
+// }
+//
+// fn my_function(x: Box<dyn MyTrait>) -> Box<dyn MyTrait> {
+//     x.f()
+// }
+//
+// fn main() {
+//     my_function(Box::new(13_u32));
+//     my_function(Box::new(String::from("abc")));
+//
+//     println!("Success!")
+// }
+//
+// trait MyTrait {
+//     fn f(&self) -> Self;
+// }
+//
+// impl MyTrait for u32 {
+//     fn f(&self) -> Self {
+//         42
+//     }
+// }
+//
+// impl MyTrait for String {
+//     fn f(&self) -> Self {
+//         self.clone()
+//     }
+// }
+//
+// fn my_function(x: impl MyTrait) -> impl MyTrait {
+//     x.f()
+// }
+//
+// fn main() {
+//     my_function(13_u32);
+//     my_function(String::from("abc"));
+//
+//     println!("Success!")
+// }
