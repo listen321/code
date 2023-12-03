2e739e5f13d3bef7d53636ff98f23b341df9cc49
listen321 <153276853@qq.com>
2023-12-03 22:40:38 +0800

    add

--- src/main.rs
+++ src/main.rs
@@ -2861,3 +2861,631 @@
 //
 //     println!("Success!")
 // }
+
+//******
+//******
+//******
+// 10.5.1
+// struct Container(i32, i32);
+//
+// // 使用关联类型实现重新实现以下特征
+// // trait Contains {
+// //    type A;
+// //    type B;
+//
+// trait Contains {
+//     type A;
+//     type B;
+//     fn contains(&self, _: &Self::A, _: &Self::B) -> bool;
+//     fn first(&self) -> i32;
+//     fn last(&self) -> i32;
+// }
+//
+// impl Contains for Container {
+//     type A = i32;
+//     type B = i32;
+//     fn contains(&self, number_1: &i32, number_2: &i32) -> bool {
+//         (&self.0 == number_1) && (&self.1 == number_2)
+//     }
+//     // Grab the first number.
+//     fn first(&self) -> i32 {
+//         self.0
+//     }
+//
+//     // Grab the last number.
+//     fn last(&self) -> i32 {
+//         self.1
+//     }
+// }
+//
+// fn difference(container: &impl Contains) -> i32 {
+//     container.last() - container.first()
+// }
+//
+// fn main() {
+//     let number_1 = 3;
+//     let number_2 = 10;
+//
+//     let container = Container(number_1, number_2);
+//
+//     println!(
+//         "Does container contain {} and {}: {}",
+//         &number_1,
+//         &number_2,
+//         container.contains(&number_1, &number_2)
+//     );
+//     println!("First number: {}", container.first());
+//     println!("Last number: {}", container.last());
+//
+//     println!("The difference is: {}", difference(&container));
+// }
+
+//******
+//******
+//******
+// 10.5.2
+// use std::ops::Sub;
+//
+// #[derive(Debug, PartialEq)]
+// struct Point<T> {
+//     x: T,
+//     y: T,
+// }
+//
+// 用三种方法填空: 其中两种使用默认的泛型参数，另外一种不使用
+// impl<T: Sub<Output = T>> Sub<Point<T>> for Point<T> {
+//     type Output = Self;
+//
+//     fn sub(self, other: Self) -> Self::Output {
+//         Point {
+//             x: self.x - other.x,
+//             y: self.y - other.y,
+//         }
+//     }
+// }
+// impl<T: Sub<Output = T>> Sub<Self> for Point<T> {
+//     type Output = Self;
+//
+//     fn sub(self, other: Self) -> Self::Output {
+//         Point {
+//             x: self.x - other.x,
+//             y: self.y - other.y,
+//         }
+//     }
+// }
+// impl<T: Sub<Output = T>> Sub for Point<T> {
+//     type Output = Self;
+//
+//     fn sub(self, other: Self) -> Self::Output {
+//         Point {
+//             x: self.x - other.x,
+//             y: self.y - other.y,
+//         }
+//     }
+// }
+//
+// fn main() {
+//     assert_eq!(
+//         Point { x: 2, y: 3 } - Point { x: 1, y: 0 },
+//         Point { x: 1, y: 3 }
+//     );
+//
+//     println!("Success!")
+// }
+
+//******
+//******
+//******
+// 10.5.3
+
+// trait UsernameWidget {
+//     fn get(&self) -> String;
+// }
+//
+// trait AgeWidget {
+//     fn get(&self) -> u8;
+// }
+//
+// struct Form {
+//     username: String,
+//     age: u8,
+// }
+//
+// impl UsernameWidget for Form {
+//     fn get(&self) -> String {
+//         self.username.clone()
+//     }
+// }
+//
+// impl AgeWidget for Form {
+//     fn get(&self) -> u8 {
+//         self.age
+//     }
+// }
+//
+// fn main() {
+//     let form = Form {
+//         username: "rustacean".to_owned(),
+//         age: 28,
+//     };
+//
+//     // 如果你反注释下面一行代码，将看到一个错误: Fully Qualified Syntax
+//     // 毕竟，这里有好几个同名的 `get` 方法
+//     //
+//     // println!("{}", form.get());
+//
+//     let username = UsernameWidget::get(&form);
+//     assert_eq!("rustacean".to_owned(), username);
+//     let age = AgeWidget::get(&form); // 你还可以使用以下语法 `<Form as AgeWidget>::get`
+//     assert_eq!(28, age);
+//
+//     println!("Success!")
+// }
+
+// trait Pilot {
+//     fn fly(&self) -> String;
+// }
+//
+// trait Wizard {
+//     fn fly(&self) -> String;
+// }
+//
+// struct Human;
+//
+// impl Pilot for Human {
+//     fn fly(&self) -> String {
+//         String::from("This is your captain speaking.")
+//     }
+// }
+//
+// impl Wizard for Human {
+//     fn fly(&self) -> String {
+//         String::from("Up!")
+//     }
+// }
+//
+// impl Human {
+//     fn fly(&self) -> String {
+//         String::from("*waving arms furiously*")
+//     }
+// }
+//
+// fn main() {
+//     let person = Human;
+//
+//     assert_eq!(Pilot::fly(&person), "This is your captain speaking.");
+//     assert_eq!(Wizard::fly(&person), "Up!");
+//
+//     assert_eq!(person.fly(), "*waving arms furiously*");
+//
+//     println!("Success!")
+// }
+
+//******
+//******
+//******
+// 10.5.4
+
+//******
+//******
+//******
+// 10.5.3
+
+// trait Person {
+//     fn name(&self) -> String;
+// }
+//
+// // Person 是 Student 的 supertrait .
+// // 实现 Student 需要同时实现 Person.
+// trait Student: Person {
+//     fn university(&self) -> String;
+// }
+//
+// trait Programmer {
+//     fn fav_language(&self) -> String;
+// }
+//
+// // CompSciStudent (computer science student) 是 Programmer
+// // 和 Student 的 subtrait. 实现 CompSciStudent 需要先实现这两个 supertraits.
+// trait CompSciStudent: Programmer + Student {
+//     fn git_username(&self) -> String;
+// }
+//
+// fn comp_sci_student_greeting(student: &dyn CompSciStudent) -> String {
+//     format!(
+//         "My name is {} and I attend {}. My favorite language is {}. My Git username is {}",
+//         student.name(),
+//         student.university(),
+//         student.fav_language(),
+//         student.git_username()
+//     )
+// }
+//
+// struct CSStudent {
+//     name: String,
+//     university: String,
+//     fav_language: String,
+//     git_username: String,
+// }
+//
+// // 为 CSStudent 实现所需的特征
+// impl CompSciStudent for CSStudent {
+//     fn git_username(&self) -> String {
+//         return self.git_username.clone();
+//     }
+// }
+//
+// impl Person for CSStudent {
+//     fn name(&self) -> String {
+//         return self.name.clone();
+//     }
+// }
+//
+// impl Student for CSStudent {
+//     fn university(&self) -> String {
+//         return self.university.clone();
+//     }
+// }
+//
+// impl Programmer for CSStudent {
+//     fn fav_language(&self) -> String {
+//         return self.fav_language.clone();
+//     }
+// }
+//
+// fn main() {
+//     let student = CSStudent {
+//         name: "Sunfei".to_string(),
+//         university: "XXX".to_string(),
+//         fav_language: "Rust".to_string(),
+//         git_username: "sunface".to_string(),
+//     };
+//
+//     // 填空
+//     println!("{}", comp_sci_student_greeting(&student));
+// }
+
+//******
+//******
+//******
+// 10.5.4
+
+// use std::fmt;
+//
+// // 定义一个 newtype `Pretty`
+// struct Pretty(String);
+//
+// impl fmt::Display for Pretty {
+//     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+//         write!(f, "\"{}\"", self.0.clone() + ", world")
+//     }
+// }
+//
+// fn main() {
+//     let w = Pretty("hello".to_string());
+//     println!("w = {}", w);
+// }
+
+//******
+//******
+//******
+// 10.5.5
+
+// struct Container(i32, i32);
+//
+// // 使用关联类型实现重新实现以下特征
+// trait Contains {
+//     type A;
+//     type B;
+//     fn contains(&self, _: &Self::A, _: &Self::B) -> bool;
+//     fn first(&self) -> i32;
+//     fn last(&self) -> i32;
+// }
+
+// trait Contains<A, B> {
+//     fn contains(&self, _: &A, _: &B) -> bool;
+//     fn first(&self) -> i32;
+//     fn last(&self) -> i32;
+// }
+
+// impl Contains<i32, i32> for Container {
+//     fn contains(&self, number_1: &i32, number_2: &i32) -> bool {
+//         (&self.0 == number_1) && (&self.1 == number_2)
+//     }
+//     // Grab the first number.
+//     fn first(&self) -> i32 {
+//         self.0
+//     }
+//
+//     // Grab the last number.
+//     fn last(&self) -> i32 {
+//         self.1
+//     }
+// }
+// impl Contains for Container {
+//     type A = i32;
+//     type B = i32;
+//     fn contains(&self, number_1: &i32, number_2: &i32) -> bool {
+//         (&self.0 == number_1) && (&self.1 == number_2)
+//     }
+//     // Grab the first number.
+//     fn first(&self) -> i32 {
+//         self.0
+//     }
+//
+//     // Grab the last number.
+//     fn last(&self) -> i32 {
+//         self.1
+//     }
+// }
+//
+// // fn difference<A, B, C: Contains<A, B>>(container: &C) -> i32 {
+// //     container.last() - container.first()
+// // }
+// fn difference<C: Contains>(container: &C) -> i32 {
+//     container.last() - container.first()
+// }
+//
+// fn main() {
+//     let number_1 = 3;
+//     let number_2 = 10;
+//
+//     let container = Container(number_1, number_2);
+//
+//     println!(
+//         "Does container contain {} and {}: {}",
+//         &number_1,
+//         &number_2,
+//         container.contains(&number_1, &number_2)
+//     );
+//     println!("First number: {}", container.first());
+//     println!("Last number: {}", container.last());
+//
+//     println!("The difference is: {}", difference(&container));
+// }
+
+//******
+//******
+//******
+// 10.5.5
+// use std::ops::Sub;
+//
+// #[derive(Debug, PartialEq)]
+// struct Point<T> {
+//     x: T,
+//     y: T,
+// }
+//
+// // 用三种方法填空: 其中两种使用默认的泛型参数，另外一种不使用
+// // impl<T: Sub<Output = T>> Sub<Point<T>> for Point<T> {
+// //     type Output = Self;
+// //
+// //     fn sub(self, other: Self) -> Self::Output {
+// //         Point {
+// //             x: self.x - other.x,
+// //             y: self.y - other.y,
+// //         }
+// //     }
+// // }
+// //
+// // impl<T: Sub<Output = T>> Sub<Self> for Point<T> {
+// //     type Output = Self;
+// //
+// //     fn sub(self, other: Self) -> Self::Output {
+// //         Point {
+// //             x: self.x - other.x,
+// //             y: self.y - other.y,
+// //         }
+// //     }
+// // }
+//
+// impl<T: Sub<Output = T>> Sub for Point<T> {
+//     type Output = Self;
+//
+//     fn sub(self, other: Self) -> Self::Output {
+//         Point {
+//             x: self.x - other.x,
+//             y: self.y - other.y,
+//         }
+//     }
+// }
+//
+// fn main() {
+//     assert_eq!(
+//         Point { x: 2, y: 3 } - Point { x: 1, y: 0 },
+//         Point { x: 1, y: 3 }
+//     );
+//
+//     println!("Success!")
+// }
+
+//******
+//******
+//******
+// 10.5.5
+
+// trait UsernameWidget {
+//     fn get(&self) -> String;
+// }
+//
+// trait AgeWidget {
+//     fn get(&self) -> u8;
+// }
+//
+// struct Form {
+//     username: String,
+//     age: u8,
+// }
+//
+// impl UsernameWidget for Form {
+//     fn get(&self) -> String {
+//         self.username.clone()
+//     }
+// }
+//
+// impl AgeWidget for Form {
+//     fn get(&self) -> u8 {
+//         self.age
+//     }
+// }
+//
+// fn main() {
+//     let form = Form {
+//         username: "rustacean".to_owned(),
+//         age: 28,
+//     };
+//
+//     // 如果你反注释下面一行代码，将看到一个错误: Fully Qualified Syntax
+//     // 毕竟，这里有好几个同名的 `get` 方法
+//     //
+//     // println!("{}", form.get());
+//
+//     let username = UsernameWidget::get(&form);
+//     assert_eq!("rustacean".to_owned(), username);
+//     let age = AgeWidget::get(&form); // 你还可以使用以下语法 `<Form as AgeWidget>::get`
+//     assert_eq!(28, age);
+//
+//     println!("Success!")
+// }
+
+// trait Person {
+//     fn name(&self) -> String;
+// }
+//
+// // Person 是 Student 的 supertrait .
+// // 实现 Student 需要同时实现 Person.
+// trait Student: Person {
+//     fn university(&self) -> String;
+// }
+//
+// trait Programmer {
+//     fn fav_language(&self) -> String;
+// }
+//
+// // CompSciStudent (computer science student) 是 Programmer
+// // 和 Student 的 subtrait. 实现 CompSciStudent 需要先实现这两个 supertraits.
+// trait CompSciStudent: Programmer + Student {
+//     fn git_username(&self) -> String;
+// }
+//
+// fn comp_sci_student_greeting(student: &dyn CompSciStudent) -> String {
+//     format!(
+//         "My name is {} and I attend {}. My favorite language is {}. My Git username is {}",
+//         student.name(),
+//         student.university(),
+//         student.fav_language(),
+//         student.git_username()
+//     )
+// }
+//
+// struct CSStudent {
+//     name: String,
+//     university: String,
+//     fav_language: String,
+//     git_username: String,
+// }
+//
+// // 为 CSStudent 实现所需的特征
+// impl Person for CSStudent {
+//     fn name(&self) -> String {
+//         self.name.clone()
+//     }
+// }
+//
+// impl Student for CSStudent {
+//     fn university(&self) -> String {
+//         self.university.clone()
+//     }
+// }
+//
+// impl Programmer for CSStudent {
+//     fn fav_language(&self) -> String {
+//         self.fav_language.clone()
+//     }
+// }
+//
+// impl CompSciStudent for CSStudent {
+//     fn git_username(&self) -> String {
+//         self.git_username.clone()
+//     }
+// }
+//
+// fn main() {
+//     let student = CSStudent {
+//         name: "Sunfei".to_string(),
+//         university: "XXX".to_string(),
+//         fav_language: "Rust".to_string(),
+//         git_username: "sunface".to_string(),
+//     };
+//
+//     // 填空
+//     println!("{}", comp_sci_student_greeting(&student));
+// }
+
+//******
+//******
+//******
+// 10.5.6
+
+// use std::fmt;
+//
+// // 定义一个 newtype `Pretty`
+// struct Pretty(String);
+//
+// impl fmt::Display for Pretty {
+//     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+//         write!(f, "\"{}\"", self.0.clone() + ", world")
+//     }
+// }
+//
+// fn main() {
+//     let w = Pretty("hello".to_string());
+//     println!("w = {}", w);
+// }
+
+//******
+//******
+//******
+// 11.1.1
+
+// 填空并修复错误
+// 1. 不要使用 `to_string()`
+// 2. 不要添加/删除任何代码行
+// fn main() {
+//     let mut s: String = String::from("hello, ");
+//     s.push_str("world");
+//     s.push('!');
+//
+//     let e = s.clone();
+//     move_ownership(s);
+//
+//     assert_eq!(e, "hello, world!");
+//
+//     println!("Success!")
+// }
+//
+// fn move_ownership(s: String) {
+//     println!("ownership of \"{}\" is moved here!", s)
+// }
+
+//******
+//******
+//******
+// 11.1.2
+
+// 填空
+fn main() {
+    let mut s = String::from("hello, world");
+
+    let slice1: &str = &s; // 使用两种方法
+    assert_eq!(slice1, "hello, world");
+
+    let slice2 = &s[0..5];
+    assert_eq!(slice2, "hello");
+
+    let slice3: &mut String = &mut s;
+    slice3.push('!');
+    assert_eq!(slice3, "hello, world!");
+
+    println!("Success!")
+}
